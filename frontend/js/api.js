42be42ff93f9cddb687c26f45eb7b441a75d1f4c
Mohanraj111789 <kit27.am30@gmail.com>
2025-11-14 11:17:38 +0530

    fron

---
+++ frontend/js/api.js
@@ -0,0 +1,36 @@
+const API_BASE = "http://localhost:8000"; // change to your server IP
+
+// ----- GET SENSOR VALUES -----
+async function getMoisture() {
+    return fetch(`${API_BASE}/sensor/moisture`).then(res => res.json());
+}
+
+async function getHumidity() {
+    return fetch(`${API_BASE}/sensor/humidity`).then(res => res.json());
+}
+
+async function getLDR() {
+    return fetch(`${API_BASE}/sensor/ldr`).then(res => res.json());
+}
+
+async function getWaterStatus() {
+    return fetch(`${API_BASE}/sensor/water`).then(res => res.json());
+}
+
+async function getMode() {
+    return fetch(`${API_BASE}/switch/mode`).then(res => res.json());
+}
+
+// ----- UPDATE MODE -----
+async function updateMode(mode) {
+    return fetch(`${API_BASE}/switch/mode/${mode}`, {
+        method: "POST"
+    }).then(res => res.json());
+}
+
+// ----- MANUAL SWITCH -----
+async function updateManualState(state) {
+    return fetch(`${API_BASE}/switch/manual/${state}`, {
+        method: "POST"
+    }).then(res => res.json());
+}
---
+++ frontend/js/main.js
@@ -0,0 +1,22 @@
+async function updateDashboard() {
+    try {
+        const moisture = await getMoisture();
+        const humidity = await getHumidity();
+        const ldr = await getLDR();
+        const water = await getWaterStatus();
+        const mode = await getMode();
+
+        document.getElementById("moistureValue").innerText = moisture.value + " %";
+        document.getElementById("humidityValue").innerText = humidity.value + " %";
+        document.getElementById("ldrValue").innerText = ldr.value;
+        document.getElementById("waterStatus").innerText = water.status ? "Water Present" : "No Water";
+        document.getElementById("modeStatus").innerText = mode.mode;
+
+    } catch (e) {
+        console.log("Error fetching data:", e);
+    }
+}
+
+// Refresh every 2 seconds
+setInterval(updateDashboard, 2000);
+updateDashboard();
